da18fc31e99bda2ce2aa35535df04038bb4bb9c3
ueki397 <uekijunh522@gmail.com>
2021-02-20 21:03:52 +0900

    4-2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get "pages/home"以下のように書き換えられる。(URLがhttp://localhost:3000/になる。)
   root "pages#home"
+
+  get "/users/:id", to: "users#show", as:"user"
 end
