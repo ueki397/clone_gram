b5c21884fbce98da937c6c9d48931ed3846377b9
ueki397 <uekijunh522@gmail.com>
2021-02-20 15:03:39 +0900

    new

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get "pages/home"以下のように書き換えられる。(URLがhttp://localhost:3000/になる。)
+  root "pages#home"
+end
