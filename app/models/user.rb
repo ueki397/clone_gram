855dae653280ad2faf63cd558a7d43c0b17d0a00
ueki397 <uekijunh522@gmail.com>
2021-02-22 21:54:20 +0900

    8-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :likes
+  has_many :comments
 
   devise :database_authenticatable, :registerable, :recoverable,:rememberable, :validatable
   validates :name,presence: true,length: {maximum: 50 }
